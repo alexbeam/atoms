fb0c69e53ce0a88f4523e68a4e01a53a55bf14a0
alexbeam <beam@0587390258.wireless.umich.net>
2014-10-30 17:44:42 -0400

    Again

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,20 @@ Bundler.require(*Rails.groups)
 module Atoms
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    
+    initializer 'setup_asset_pipeline', :group => :all  do |app|
+      # We don't want the default of everything that isn't js or css, because it pulls too many things in
+      app.config.assets.precompile.shift
+
+      # Explicitly register the extensions we are interested in compiling
+      app.config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+          '.html', '.erb', '.haml',                 # Templates
+          '.png',  '.gif', '.jpg', '.jpeg',         # Images
+          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
+      end)
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
